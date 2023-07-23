99bf72f8d9f3f031fccf741986f1e2daeede2cb5
uniS2 <s2oie1010@gmail.com>
2023-07-23 21:00:08 +0900

    update. css.js 에러 수정

--- poster/client/lib/dom/css.js
+++ poster/client/lib/dom/css.js
@@ -5,7 +5,6 @@ export { addClass, removeClass };
 
 function addClass(node, className) {
   if(isString(node)) node = getNode(node);
-  // if(!isString(className)) typeError('addClass 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
   if(!className) newError('addClass 함수의 두 번째 인수는 필수 값 이어야 합니다.');
   node.classList.add(className);
 }
@@ -16,7 +15,6 @@ function removeClass(node, className) {
     node.className = "";
     return;
   }
-  // if(isString(className)) typeError('removeClass 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
   if(!className) newError('addClass 함수의 두 번째 인수는 필수 값 이어야 합니다.');
   node.classList.remove(className);
 }
